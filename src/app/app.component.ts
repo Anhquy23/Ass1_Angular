5b47ecfa49e0f2b3ca0ed3d69c63ac9f88c076ae
Anhquy23 <Ttm1022013@gmail.com>
2023-05-27 10:24:01 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { IProduct } from 'src/interface/Product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ass1_angular';
+  
 }
